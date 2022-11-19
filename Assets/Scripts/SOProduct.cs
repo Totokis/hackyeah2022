cbe7f281e3be200c2ce8940d4c0506681af5a6ef
astisk <pobierac@gmail.com>
2022-11-19 20:54:28 +0100

    SOProduct more

--- Assets/Scripts/SOProduct.cs
+++ Assets/Scripts/SOProduct.cs
@@ -2,6 +2,7 @@ using Assets.Scripts;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class SOProduct : ScriptableObject
@@ -10,4 +11,17 @@ public class SOProduct : ScriptableObject
     public Sprite Icon;
     public GameObject Prefab;
     public String Audio;
+
+    [MenuItem("Assets/Create/SOProduct")]
+    public static void CreateMyAsset()
+    {
+        SOProduct asset = ScriptableObject.CreateInstance<SOProduct>();
+
+        AssetDatabase.CreateAsset(asset, "Assets/SOProduct.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
 }
