487542e6172a4ed4c48eeb6b13732e346cb40b47
astisk <pobierac@gmail.com>
2022-11-19 20:50:59 +0100

    SOProduct basic information

--- Assets/Scripts/SOProduct.cs
+++ Assets/Scripts/SOProduct.cs
@@ -1,8 +1,13 @@
+using Assets.Scripts;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SOProduct : ScriptableObject
 {
-
+    public ProductKind ProductKind;
+    public Sprite Icon;
+    public GameObject Prefab;
+    public String Audio;
 }
