5941f4bea320d3f545b49561336dc7dbe0063133
Totokis <pawelj_janusz@wp.pl>
2022-11-20 00:02:44 +0100

    day controller

--- Assets/Scripts/DayController.cs
+++ Assets/Scripts/DayController.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 
 public class DayController : MonoBehaviour
 {
+    [SerializeField] private float dayValue;
+    [Space]
+    [SerializeField] private float lightTargetRotationY;
+    [SerializeField] private float lightTargetRotationX;
+    [SerializeField] private float skyboxRotationTargetValue;
     [SerializeField] private Light light;
+    [SerializeField] private Material skybox;
+    
     private float _cachedLightValueRotationX;
     private float _cachedLightValueRotationY;
+    private float _cachedSkyboxRotation;
 
     private void Awake()
     {
-        _cachedLightValueRotationX = light.transform.rotation.x;
-        _cachedLightValueRotationY= light.transform.rotation.y;
+        _cachedLightValueRotationX = light.transform.eulerAngles.x;
+        _cachedLightValueRotationY= light.transform.eulerAngles.y;
+        _cachedSkyboxRotation = skybox.GetFloat("_Rotation");
     }
     private void Update()
     {
+        var x = Mathf.Clamp(dayValue, lightTargetRotationX,_cachedLightValueRotationX);
+        var y = Mathf.Clamp(dayValue,lightTargetRotationY, _cachedLightValueRotationY );
+        var skyboxRotation = Mathf.Clamp(dayValue, skyboxRotationTargetValue,_cachedSkyboxRotation );
+        
+        light.transform.rotation = Quaternion.Euler(x,y,light.transform.eulerAngles.z);
+        skybox.SetFloat("_Rotation",skyboxRotation);
         
     }
 }
