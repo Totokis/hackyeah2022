5fc3371f6edda7e6fd403af5f34b7fc4159ed9fe
astisk <pobierac@gmail.com>
2022-11-19 17:06:43 +0100

    Customer timer

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,12 +19,30 @@ namespace Assets.Scripts
             RequestedProducts = OrderManager.Instance.GetOrder();
             RemainingTime = 60f;
 
+            StartCoroutine(TimePasses());
+
             #region TEST
             //String logText = "Generated products: ";
             //foreach (var product in RequestedProducts)
             //    logText += $"{product.ToString()} ";
             //Debug.Log(logText);
             #endregion TEST
+        }
+
+        private IEnumerator TimePasses()
+        {
+            while (RemainingTime > 0f)
+            {
+                RemainingTime -= 1f;
+                yield return new WaitForSeconds(1);
+            }
+
+            Leave();
+        }
+
+        private void Leave()
+        {
+            Debug.Log("Customer leaves");
         }
 
     }
