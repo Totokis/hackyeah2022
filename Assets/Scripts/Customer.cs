3af50ad38685d1fb2b4ade7f2bbc47d54527925a
astisk <pobierac@gmail.com>
2022-11-20 05:06:11 +0100

    Customer insteractable

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -9,7 +9,7 @@ using UnityEngine.Events;
 
 namespace Assets.Scripts
 {
-    public class Customer : MonoBehaviour, INPCReference
+    public class Customer : MonoBehaviour, INPCReference, IInteractionTarget, IInteractionTargetG
     {
         public SOProduct[] RequestedProducts { get; private set; }
         public SOProductGroup AllProducts { get; private set; }
@@ -82,6 +82,16 @@ namespace Assets.Scripts
         public void Init(NPC npc)
         {
             this.NPC = npc;
+        }
+
+        public void Interact()
+        {
+            //tutaj info o tym co chce
+        }
+
+        public void InteractG()
+        {
+            //tutaj oddanie requesta
         }
     }
 }
