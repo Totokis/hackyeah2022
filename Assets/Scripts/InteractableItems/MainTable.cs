a27c693a628de2576c8d50fb1e8767a8b18bacd5
astisk <pobierac@gmail.com>
2022-11-20 03:55:18 +0100

    MainTable

--- Assets/Scripts/InteractableItems/MainTable.cs
+++ Assets/Scripts/InteractableItems/MainTable.cs
@@ -18,9 +18,11 @@ public class MainTable : MonoBehaviour, IInteractionTarget
     }
 
     private void PutItemOnTable(SOProduct soProduct)
-    {//000
-        //skala
-        Products.Add(Instantiate(soProduct.PrefabInHand, new Vector3(0, 0, 0), Quaternion.identity, ItemPlaceholders[Products.Count].transform));
+    {
+        GameObject newProduct = Instantiate(soProduct.PrefabInHand, new Vector3(0, 0, 0), Quaternion.identity, ItemPlaceholders[Products.Count].transform);
+        newProduct.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        newProduct.transform.localPosition = new Vector3(0, 0, 0);
+        Products.Add(newProduct);
         SOProducts.Add(soProduct);
     }
 
