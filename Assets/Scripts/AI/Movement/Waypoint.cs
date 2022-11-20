677ad27b2a0daa3e729cddf6f748be5fa58e71c8
Michał Karcz <michal.karcz.00@gmail.com>
2022-11-20 03:58:06 +0100

    waypoioio

--- Assets/Scripts/AI/Movement/Waypoint.cs
+++ Assets/Scripts/AI/Movement/Waypoint.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.EditorTools;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class Waypoint : MonoBehaviour
 {
@@ -45,4 +49,28 @@ public class Waypoint : MonoBehaviour
 
         return Vector3.Distance(transform.position, Owner.transform.position) <= range;
     }
+
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("Tools/AlignWaypointsWithNavmesh", priority = -1000)]
+    private static void AlignWaypointsWithNavmesh()
+    {
+        var waypoints = new List<Waypoint>();
+
+        foreach (var root in SceneManager.GetActiveScene().GetRootGameObjects())
+        {
+            waypoints.AddRange(root.GetComponentsInChildren<Waypoint>());
+        }
+
+        for (int i = 0; i < waypoints.Count; i++)
+        {
+            if (NavMesh.SamplePosition(waypoints[i].transform.position, out NavMeshHit hit, 100f, NavMesh.AllAreas))
+            {
+                waypoints[i].transform.position = hit.position;
+            }
+        }
+
+        EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+        Debug.Log($"Aligned {waypoints.Count} waypoints");
+    }
+#endif
 }
