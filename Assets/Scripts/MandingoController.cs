da10eefd040a13d4a49920f99e3142db93b81cfc
Łukasz Mółka <lukasz.molka5@gmail.com>
2022-11-20 07:40:23 +0100

    Mandingo solutions working

--- Assets/Scripts/MandingoController.cs
+++ Assets/Scripts/MandingoController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using FMODUnity;
+using Cysharp.Threading.Tasks;
 
 public class MandingoController : MonoBehaviour
 {
@@ -12,8 +13,32 @@ public class MandingoController : MonoBehaviour
     [SerializeField] private MandingoData _eee;
     [SerializeField] private MandingoData _thx;
 
+    private StudioEventEmitter _emitter;
+
     private void Start()
     {
+        _emitter = GetComponent<StudioEventEmitter>();
+        Test();
+    }
 
+    private async UniTask Test()
+    {
+        await PlayEvent(_serwus);
+        await PlayEvent(_proschem);
+        await PlayEvent(_pivo);
+        await PlayEvent(_vino);
+        await PlayEvent(_riba);
+        await PlayEvent(_dzie);
+        await PlayEvent(_eee);
+        await PlayEvent(_thx);
+    }
+
+    public async UniTask PlayEvent(MandingoData data)
+    {
+        _emitter.Stop();
+        _emitter.EventReference = data.EventReference;
+        _emitter.Play();
+
+        await UniTask.Delay(data.Duration);
     }
 }
--- Assets/Scripts/MandingoData.cs
+++ Assets/Scripts/MandingoData.cs
@@ -6,6 +6,9 @@ using FMODUnity;
 [CreateAssetMenu(fileName = "MandingoData", menuName = "MandingoSolutions/MandingoData")]
 public class MandingoData : ScriptableObject
 {
+    public EventReference EventReference => _event;
+    public int Duration => _duration;
+
     [SerializeField] private EventReference _event;
     [SerializeField, Range(100, 5000)] private int _duration = 1000;
 }
